abd233de6866f38066ce06c388bda07de01854da
Kubikrubik13 <118103066+Kubikrubik13@users.noreply.github.com>
2023-03-07 23:15:59 +0300

    Update surfacehelpfile.py

--- surfacehelpfile.py
+++ surfacehelpfile.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from pygame import Surface
 import os
 
@@ -11,19 +12,20 @@ def load_image(name, colorkey=None):
     image = pygame.image.load(fullname)
     return image
 
-
+width, height = 200, 200
 def firstwindow_draw():
-    size = width, height = 200, 200
+    size = width, height
     screen = pygame.display.set_mode(size)
     screen.blit(load_image('background.png'), [0, 0])
+
 #   Заголовок:
     font = pygame.font.Font(None, 50)
     head_text = font.render("Погоня за всероссом", True, (100, 255, 100))
     head_text_x = 1400 - head_text.get_width() // 2
     head_text_y = 1600 - head_text.get_height() // 2
     screen.blit(head_text, (head_text_x, head_text_y))
-    pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
-                                           text_w + 20, text_h + 20), 3)
+    pygame.draw.rect(screen, (0, 255, 0), (head_text.get_width() - 10, head_text.get_height() - 10,
+                                           head_text_x + 20, head_text_y + 20), 3)
 
 #   Регион:
     font = pygame.font.Font(None, 50)
@@ -46,3 +48,8 @@ def firstwindow_draw():
     head_text_x = 2100 - head_text.get_width() // 2
     head_text_y = 876 + head_text.get_height() // 2
     screen.blit(head_text, (head_text_x, head_text_y))
+
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONUP:
+            if pygame.MOUSEBUTTONUP[0][0] <= width / 2:
+                screen.fill(255, 255, 255)
