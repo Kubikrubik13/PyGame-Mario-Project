d324a7209a4009e7fdc76b0551a093867436aed5
Kubikrubik13 <118103066+Kubikrubik13@users.noreply.github.com>
2023-03-12 23:46:29 +0300

    Add files via upload

--- surfacehelpfile.py
+++ surfacehelpfile.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from pygame import Surface
+from const import WIDTH, HEIGHT
 import os
 
 
@@ -13,39 +14,43 @@ def load_image(name, colorkey=None):
     return image
 
 
-width, height = 1200, 600
-
-
 def firstwindow_draw():
-    ## Никаких абсолютных координат, только от width, height
-    size = width, height
+    ## Никаких абсолютных координат, только от WIDTH, HEIGHT
+    lvl = 0
+    pygame.init()
+    running = True
+    size = WIDTH, HEIGHT
     screen = pygame.display.set_mode(size)
-    screen.blit(load_image('background.png'), [0, 0])
+    screen.fill((255, 255, 255))
+    image_bg = load_image('background.png')
+    BG_IMAGE_SIZE = (WIDTH, HEIGHT)
+    image_bg = pygame.transform.scale(image_bg, BG_IMAGE_SIZE)
+    screen.blit(image_bg, [0, 0])
 
-#   Заголовок:
+    #   Заголовок:
     font = pygame.font.Font(None, 50)
-    head_text = font.render("Погоня за всероссом", True, (100, 255, 100))
-    head_text_x = width // 2 - head_text.get_width() // 2
-    head_text_y = height // 2 - head_text.get_height() // 2
-    screen.blit(head_text, (head_text_x, head_text_y))
-    pygame.draw.rect(screen, (0, 255, 0), (head_text.get_width() - 10, head_text.get_height() - 10,
+    head_text_mid = font.render("Погоня за всероссом", True, (100, 255, 100))
+    head_text_x = WIDTH // 2 - head_text_mid.get_width() // 2
+    head_text_y = HEIGHT // 2
+    screen.blit(head_text_mid, (head_text_x, head_text_y))
+    pygame.draw.rect(screen, (0, 255, 0), (head_text_mid.get_width() - 10, head_text_mid.get_height() - 10,
                                            head_text_x + 20, head_text_y + 20), 3)
 
-#   Регион:
+    #   Регион:
     font = pygame.font.Font(None, 50)
-    head_text = font.render("Регион", True, (100, 255, 100))
-    head_text_x = 700 - head_text.get_width() // 2
-    head_text_y = 876 + head_text.get_height() // 2
-    screen.blit(head_text, (head_text_x, head_text_y))
+    head_text_lf = font.render("Регион", True, (100, 255, 100))
+    head_text_x = (WIDTH // 2) // 4
+    head_text_y = HEIGHT // 2 + head_text_lf.get_height() // 2 - 20
+    screen.blit(head_text_lf, (head_text_x, head_text_y))
 
-#   Закл:
+    #   Закл:
     font = pygame.font.Font(None, 50)
-    head_text = font.render("Закл", True, (100, 255, 100))
-    head_text_x = 2100 - head_text.get_width() // 2
-    head_text_y = 876 + head_text.get_height() // 2
-    screen.blit(head_text, (head_text_x, head_text_y))
+    head_text_rg = font.render("Закл", True, (100, 255, 100))
+    head_text_x = (WIDTH // 2) * 55 // 32
+    head_text_y = HEIGHT // 2 + head_text_rg.get_height() // 2 - 20
+    screen.blit(head_text_rg, (head_text_x, head_text_y))
 
-#   Инструкция:
+    #   Инструкция:
     font = pygame.font.Font(None, 50)
     head_text = font.render('Для уровня "Регион" /n нажмите на левую часть экрана, /n для уровня "Закл" - на правую.',
                             True, (100, 255, 100))
@@ -53,8 +58,19 @@ def firstwindow_draw():
     head_text_y = 876 + head_text.get_height() // 2
     screen.blit(head_text, (head_text_x, head_text_y))
 
-    for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONUP:
-            if pygame.MOUSEBUTTONUP[0][0] <= width / 2:
-                screen.fill(255, 255, 255)
-                
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONUP:
+                if pygame.MOUSEBUTTONUP[0][0] <= WIDTH / 2:
+                    screen.fill(255, 255, 255)
+                    lvl = 1
+                if pygame.MOUSEBUTTONUP[0][0] >= WIDTH / 2:
+                    screen.fill(255, 255, 255)
+                    lvl = 2
+                elif event.type == pygame.QUIT:
+                    running = False
+            pygame.display.update()
+
+    return lvl
+
+firstwindow_draw()
